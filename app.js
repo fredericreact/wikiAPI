91dc68e5a1b6a821ef7884178061b57eeba735fd
unknown <fredericreact@gmail.com>
2022-07-07 22:51:55 +0800

    GET route at the /articles route

--- app.js
+++ app.js
@@ -14,12 +14,23 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB");
 
-const articlesSchema = {
+const articleSchema = {
   title: String,
   content : String
 }
 
-const Article = mongoose.model("Article", articlesSchema);
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function(req,res){
+  Article.find(function(err, foundArticles){
+    if(!err){
+        res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
